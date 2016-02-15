d40cfe7349b25ea679e8b2f2fb509d087ab3516b
dignifiedquire <dignifiedquire@gmail.com>
2016-02-15 01:36:30 +0100

    Api cleanup and docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # karma-peer
+
+> Test peer to peer communication between browsers using karma.
+
+
+## API
+
+There is an object `peer` exposed on `window` which gives you the following
+
+* `peer.ready(cb)`: This should be run to ensure all browsers are in sync before each test is started.
+* `peer.done`: This should be run after every test to clean up state.
+* `peer.send(msg)`: This is used to send a message to all other browsers. `msg` is an object.
+* `peer.socket`: The underlying socket.io socket.
--- test/index.spec.js
+++ test/index.spec.js
@@ -20,36 +20,31 @@ function plan (count, done) {
 describe('peer', function () {
   this.timeout(20000)
 
-  it('sum', (done) => {
-    const finish = plan(3, () => {
-      console.log('REMOVING')
-      socket.removeAllListeners('/peer/msg')
-      done()
-    })
+  beforeEach((done) => {
+    peer.ready(done)
+  })
 
+  afterEach(() => {
+    peer.done()
+  })
+
+  it('sum', (done) => {
+    const finish = plan(2, done)
     const msgId = Math.random()
 
-    socket.once('/peer/browsersready', () => {
-      console.log('  browsersready', socket.id)
-
-      console.log('  Sending sum')
-      socket.emit('/peer/msg', {
-        id: socket.id,
-        msgId: msgId,
-        method: 'sum',
-        args: [1, 2]
-      })
-      finish()
+    peer.send({
+      msgId: msgId,
+      method: 'sum',
+      args: [1, 2]
     })
 
     socket.on('/peer/msg', (msg) => {
       console.log('Recieved msg')
       if (msg.method === 'sum') {
-        console.log('  Recieved msg sum')
+        console.log('Recieved msg sum')
         const result = msg.args[0] + msg.args[1]
 
-        socket.emit('/peer/msg', {
-          id: socket.id,
+        peer.send({
           msgId: msg.msgId,
           target: msg.id,
           result: result
@@ -64,68 +59,56 @@ describe('peer', function () {
         finish()
       }
     })
-
-    socket.emit('/peer/ready')
   })
 
   it('simple webrtc', (done) => {
     console.log('starting webrtc')
-    const finish = plan(3, () => {
-      console.log('REMOVING')
-      socket.removeAllListeners('/peer/msg')
-      done()
-    })
+    const finish = plan(3, done)
 
     const isInit = !!window.parent.location.href.match(/Chrome\-1/)
-
-    socket.on('/peer/browsersready', () => {
-      console.log('ready', isInit)
-      const p = new SimplePeer({
-        initiator: isInit,
-        trickle: false
-      })
-
-      p.on('error', (err) => {
-        console.error(err)
-        throw err
-      })
-
-      socket.on('/peer/msg', (msg) => {
-        console.log('recieved')
-        if (msg.method === 'signal') {
-          p.signal(msg.data)
-        }
-      })
-
-      const sendSignal = (data) => {
-        console.log('sending')
-        socket.emit('/peer/msg', {
-          id: socket.id,
-          method: 'signal',
-          data
-        })
-      }
-
-      p.on('signal', (data) => {
-        sendSignal(data)
-        finish()
-      })
-
-      p.once('connect', () => {
-        console.log('connected, sending')
-        // wait for 'connect' event before using the data channel
-        p.send('hey peer2, how is it going?')
-        finish()
-      })
-
-      p.once('data', (data) => {
-        // got a data channel message
-        console.log('got a message from peer1: ' + data)
-        expect(data).to.be.eql('hey peer2, how is it going?')
-        finish()
-      })
+    const p = new SimplePeer({
+      initiator: isInit,
+      trickle: false
     })
 
-    socket.emit('/peer/ready')
+    p.on('error', (err) => {
+      console.error(err)
+      throw err
+    })
+
+    socket.on('/peer/msg', (msg) => {
+      console.log('recieved')
+      if (msg.method === 'signal') {
+        p.signal(msg.data)
+      }
+    })
+
+    const sendSignal = (data) => {
+      console.log('sending')
+
+      peer.send({
+        method: 'signal',
+        data
+      })
+    }
+
+    p.on('signal', (data) => {
+      sendSignal(data)
+      finish()
+    })
+
+    p.once('connect', () => {
+      console.log('connected, sending')
+      // wait for 'connect' event before using the data channel
+      p.send('hey peer2, how is it going?')
+      finish()
+    })
+
+    p.once('data', (data) => {
+      // got a data channel message
+      console.log('got a message from peer1: ' + data)
+      expect(data).to.be.eql('hey peer2, how is it going?')
+      finish()
+    })
   })
 })
